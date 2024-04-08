163d7670f9134393b7fd759754ae1b49be58d4a6
saragalko <sara.anya.galko33@gmail.com>
2024-04-08 22:04:02 +0300

    add tests auth endpoints

--- online_shop/users/urls.py
+++ online_shop/users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('activate/<str:uid>/<str:token>/', ActivateUser.as_view({'get': 'activation'})),
-    path('test-login/', TestLoginView.as_view())
+    path('test-login/', TestLoginView.as_view(), name ='test-login')
 ]
