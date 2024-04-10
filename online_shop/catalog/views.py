b1f28754965dc20311fca4ff826d3e0eea42cfdc
saragalko <sara.anya.galko33@gmail.com>
2024-04-10 22:08:12 +0300

    add swagger

--- online_shop/catalog/views.py
+++ online_shop/catalog/views.py
@@ -8,6 +8,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.db.models import F
 from catalog.tasks import some_task
+from drf_yasg.utils import swagger_auto_schema
 
 
 class CategoryListView(ListAPIView):
@@ -103,6 +104,13 @@ class CartView(APIView):
 class OrderView(APIView):
     permission_classes = (IsAuthenticated, )
 
+    @swagger_auto_schema(
+        request_method="POST",
+        request_body=OrderSerializer,
+        responses={
+            200: OrderSerializer
+        }
+    )
     def post(self, request):
         input_serializer = OrderSerializer(data=request.data, context={'request': request})
         input_serializer.is_valid(raise_exception=True)
